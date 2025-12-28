6668aab4a9921777cdce9346462035b43c819e0a
Joevan Pramana Achmad <116768440+Joevan29@users.noreply.github.com>
2025-12-28 13:31:48 +0700

    Delete setup.bat

--- setup.bat
+++
@@ -1,2 +0,0 @@
-call npm init -y
-call npm install next react react-dom framer-motion lucide-react clsx tailwind-merge typescript @types/node @types/react @types/react-dom postcss tailwindcss eslint eslint-config-next
